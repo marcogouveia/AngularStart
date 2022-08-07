9b31835c0192e238ff6a82f0017063b3655bc3e3
Marco Gouveia <marcogouveia2001@outlook.com>
2022-08-07 16:24:13 -0300

    Validações basicas de React Forms

--- src/app/components/reactive-forms/reactive-forms.component.ts
+++ src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Usuario } from './models/usuario';
+
 
 @Component({
   selector: 'app-reactive-forms',
@@ -8,24 +11,48 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
 
-  cadastroForm = new FormGroup({
-    nome: new FormControl(''),
-    cpf: new FormControl(''),
-    email: new FormControl(''),
-    senha: new FormControl(''),
-    senhaConfirmacao: new FormControl(''),
-  })
+  //Form Control / Group
 
-  constructor () {
+  // cadastroForm = new FormGroup({
+  //   nome: new FormControl(''),
+  //   cpf: new FormControl(''),
+  //   email: new FormControl(''),
+  //   senha: new FormControl(''),
+  //   senhaConfirmacao: new FormControl(''),
+  // })
+
+  // FormBuilder
+
+
+  cadastroForm: FormGroup;
+  usuario: Usuario;
+  formResult: string = "";
+
+
+  constructor (private fb: FormBuilder ) {
 
   }
 
   ngOnInit(): void {
-
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: [''],
+      email: ['', [Validators.required, Validators.email]],
+      senha: [''],
+      senhaConfirmacao: [''],
+    })
   }
 
   adicionarUsuario(){
-    let x = this.cadastroForm.value;
+
+    if(this.cadastroForm.dirty && this.cadastroForm.valid){
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value)
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    }else{
+      this.formResult = "Não Submeteu!!";
+    }
+
+
   }
 
 }
